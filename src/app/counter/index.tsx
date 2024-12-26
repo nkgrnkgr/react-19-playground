c5de516d455275b5ae1635638e25dfb6abdd5729
nkgrnkgr <beefupme@gmail.com>
2024-12-26 22:05:00 +0900

    三通りの実装

--- src/app/counter/index.tsx
+++ src/app/counter/index.tsx
@@ -1,4 +1,9 @@
-import { useActionState } from "react";
+import {
+  startTransition,
+  useActionState,
+  useState,
+  useTransition,
+} from "react";
 
 // 非同期データ取得関数
 const fetchData = async (): Promise<void> => {
@@ -9,7 +14,58 @@ const fetchData = async (): Promise<void> => {
   });
 };
 
-export function Counter() {
+export function CounterForUseTransition() {
+  const [count, setCount] = useState(0);
+  const [isPending, st] = useTransition();
+
+  const handleClick = () => {
+    st(async () => {
+      await fetchData();
+      setCount(count + 1);
+    });
+  };
+
+  return (
+    <div>
+      <h2>CounterForUseTransition</h2>
+      <p>Count: {count}</p>
+      <button type="button" onClick={handleClick} disabled={isPending}>
+        Increment
+      </button>
+    </div>
+  );
+}
+
+export function CounterForUseActionState() {
+  const [count, runAction, isPending] = useActionState<
+    number,
+    { type: "INCREMENT" }
+  >(async (prev, payload) => {
+    if (payload.type === "INCREMENT") {
+      await fetchData();
+      return prev + 1;
+    }
+    return prev;
+  }, 0);
+
+  const handleClick = () => {
+    startTransition(() => {
+      runAction({ type: "INCREMENT" });
+    });
+  };
+
+  return (
+    <div>
+      <h2>CounterFor formAction</h2>
+      <p>Count: {count}</p>
+      <button type="button" onClick={handleClick} disabled={isPending}>
+        Increment
+      </button>
+    </div>
+  );
+}
+
+export function CounterForFormAction() {
   const [count, runAction, isPending] = useActionState<
     number,
     { type: "INCREMENT" }
@@ -27,6 +83,7 @@ export function Counter() {
         runAction({ type: "INCREMENT" });
       }}
     >
+      <h2>CounterFor formAction</h2>
       <p>Count: {count}</p>
       <button type="submit" disabled={isPending}>
         Increment
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
-import { Counter } from "./counter";
+import { CounterForFormAction, CounterForUseTransition } from "./counter";
 
 export function App() {
   return (
     <div>
       <h1>Hello React 19</h1>
-      <Counter />
+      <CounterForFormAction />
+      <CounterForUseTransition />
+      <CounterForUseTransition />
     </div>
   );
 }
